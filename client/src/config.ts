30afda1574183141141f51756a155704e0246d9b
Francis254 <59084931+Frank6496@users.noreply.github.com>
2022-07-30 06:20:24 -0700

    configured our url

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,5 +4,5 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com
 export const authConfig = {
   domain: 'dev-2f-69763.us.auth0.com',            // Auth0 domain
   clientId: 'G3aiEzdkYZGyYssj0QSd2iX63uYRHpkQ',          // Auth0 client id
-  callbackUrl: 'http://localhost:3001/callback'
+  callbackUrl: 'http://localhost:3000/callback'
 };
